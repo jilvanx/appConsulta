e485c49a32b273fe330de8625d94401101f75586
jilvanx <jilvan.silva@gmail.com>
2017-09-04 17:26:55 -0400

    Ajustes no bootstrap e adicionado o arquivo de proxy.config.json

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfissionalService } from '../profissional.service'
+import { Profissional } from '../shared/profissional.model'
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,22 @@ import { ProfissionalService } from '../profissional.service'
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public profissionais: Profissional[]
+
+  constructor(private profissionalService: ProfissionalService) { }
 
   ngOnInit() {
     
+
+    this.profissionalService.getProfissionais()
+      .then((profissionais: Profissional[]) => {
+        console.log(profissionais)
+        this.profissionais = profissionais
+      })
+      .catch((param: any) => {
+        console.log(param)
+      })
+
   }
 
 }
--- src/app/profissional.service.ts
+++ src/app/profissional.service.ts
@@ -1,19 +1,36 @@
-import { Http } from '@angular/http'
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Profissional } from './shared/profissional.model'
-
 import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class ProfissionalService {
 
-    constructor(private http: Http){}
 
-    public getOfertas(): Promise<Profissional[]> {
-        //Efetuar uma requisição http
-        return this.http.get('http://www.indicoapp.com/api/v1/workers/')
+    constructor(private http : Http){}
+  
+    // public getProfissionais(): Promise<Profissional[]> {
+
+    //     // let headers = new Headers();
+    //     // headers.append('Access-Control-Allow-Origin', '*')
+    //     // headers.append('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS')
+    //     // let options = new RequestOptions({ headers: headers });        
+
+    //     //Efetuar uma requisição http
+    //     //http://indikko.herokuapp.com/api/v1/workers/?format=json&page=1
+    //     //https://cors-anywhere.herokuapp.com/indikko.herokuapp.com/api/v1/workers/?format=json&page=1
+    //     return this.http.get('https://cors-anywhere.herokuapp.com/indikko.herokuapp.com/api/v1/workers/')
+    //         .toPromise()
+    //         .then((resposta: any) => resposta.json().results as Profissional[])
+
+    // }
+
+    public getProfissionais(): Promise<Profissional[]> {
+        
+        return this.http.get('https://cors-anywhere.herokuapp.com/indikko.herokuapp.com/api/v1/workers/')
             .toPromise()
-            .then((resposta: any) => resposta.json())
-    }
+            .then((resposta: any) => resposta.json().results as Profissional[])
+        
+    }    
 
 }
